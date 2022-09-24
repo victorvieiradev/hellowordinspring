53f64e49b830fb5688c7042dc73cd394ff48dc85
victorvieiradev <contatovictorvieira@outlook.com>
2022-09-24 14:47:10 -0300

    added GreetingController

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s !";
+    private final AtomicLong counter = new AtomicLong();
+    @GetMapping("/greeting")
+    Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
